e6ecd52314ace4c1e0bf5618ca474c1e91df9534
Alex Harper <alex.harper@tactis.uk>
2025-08-26 13:21:30 +0100

    Update Caddy version to 2.10.2 in build script and Dockerfile

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 --tag tactis/caddy-azuredns:2.9.1 --tag tactis/caddy-azuredns:latest --push .
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 --tag tactis/caddy-azuredns:2.10.2 --tag tactis/caddy-azuredns:latest --push .
